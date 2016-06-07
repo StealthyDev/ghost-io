6bddd6f2fce0fd4d10db5efc5ba6bcd3b4ddaf4c
StealthyDev <StealthyDev@gmail.com>
2016-06-07 12:39:49 -0400

    Base project structure

---
+++ src/main/java/com/stealthydev/open/GhostIoApplication.java
@@ -0,0 +1,15 @@
+package com.stealthydev.open;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class GhostIoApplication extends WebMvcAutoConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhostIoApplication.class, args);
+    }
+}
