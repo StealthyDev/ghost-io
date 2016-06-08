6575ffe39ca6573aab9c1cd80a8d0adde041fcc3
StealthyDev <StealthyDev@gmail.com>
2016-06-07 23:48:19 -0400

    Minor tests

--- src/main/java/com/stealthydev/open/ghostio/service/TriggerService.java
+++ src/main/java/com/stealthydev/open/ghostio/service/TriggerService.java
@@ -1,16 +1,17 @@
 package com.stealthydev.open.ghostio.service;
 
 import com.stealthydev.open.ghostio.model.TriggerRequest;
-import com.sun.glass.events.KeyEvent;
 import org.springframework.stereotype.Service;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 @Service
 public class TriggerService {
     public void trigger(TriggerRequest triggerRequest) throws AWTException {
         Robot r = new Robot();
-        r.keyPress(KeyEvent.VK_WINDOWS);
-        r.keyRelease(KeyEvent.VK_WINDOWS);
+        int firstChar = (int) triggerRequest.getInput().charAt(0);
+        r.keyPress(KeyEvent.VK_G);
+        r.keyRelease(KeyEvent.VK_G);
     }
 }
