bafe4ab808ca7925ca878bd661eb96b8d73d114a
StealthyDev <StealthyDev@gmail.com>
2016-06-09 20:30:54 -0400

    Added autohotkey

--- src/main/java/com/stealthydev/open/ghostio/util/RobotUtil.java
+++ src/main/java/com/stealthydev/open/ghostio/util/RobotUtil.java
@@ -3,20 +3,16 @@ package com.stealthydev.open.ghostio.util;
 import com.stealthydev.open.ghostio.model.KeyMap;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class RobotUtil {
-//    IntCall
     public static void fireEvent(KeyMap keymap) throws AWTException {
-
-
-
-
-//        Robot bot = new Robot();
-////        bot.keyPress(KeyEvent.KEY_LOCATION_RIGHT);
-//        bot.keyPress(KeyEvent.VK_SHIFT);
-//        bot.keyPress(KeyEvent.VK_G);
-//        bot.keyRelease(KeyEvent.VK_G);
-//        bot.keyRelease(KeyEvent.VK_SHIFT);
-////        bot.keyRelease(KeyEvent.KEY_LOCATION_RIGHT);
+        Robot bot = new Robot();
+//        bot.keyPress(KeyEvent.KEY_LOCATION_RIGHT);
+        bot.keyPress(KeyEvent.VK_SHIFT);
+        bot.keyPress(KeyEvent.VK_G);
+        bot.keyRelease(KeyEvent.VK_G);
+        bot.keyRelease(KeyEvent.VK_SHIFT);
+//        bot.keyRelease(KeyEvent.KEY_LOCATION_RIGHT);
     }
 }
